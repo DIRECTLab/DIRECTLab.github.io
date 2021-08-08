19cfcfdd38bcb187a595b2754383075e815a84c6
johnathankunz@gmail.com <johnathan.kunz@aggiemail.usu.edu>
2021-08-08 00:02:38 -0600

    added assets, improved the header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  header_image = "assets/Logos/DIRECT Lab Logo-B.png"
+  links = [
+    "Home",
+    "Projects",
+    "Members"
+  ]
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  jump(link: string) {
+    console.log("here")
+    console.log(link)
+    if (link === "Home") {
+      this.router.navigate(["home"])
+    }
+    else if (link === "Projects") {
+      this.router.navigate(["projects"])
+    }
+    else if (link === "Members") {
+      this.router.navigate(["members"])
+    }
+  }
+
+}
