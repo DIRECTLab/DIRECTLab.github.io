744e30aae0e84e81b25a0da8898811f1826c5424
Carter Bailey <carterbailey@protonmail.com>
2021-08-15 13:15:31 -0600

    adding bio and photo

--- src/app/lab-members/all-members.ts
+++ src/app/lab-members/all-members.ts
@@ -2,6 +2,6 @@ import { Member } from "./member";
 
 export const allMembers: Member[] = [
   new Member("John", "assets/MemberImages/John.jpg", "A Cs Student"),
-  new Member("John", "assets/MemberImages/John.jpg", "A Cs Student"),
+  new Member("Carter", "assets/MemberImages/Carter.jpg", "An undergraduate student pursuing a Bachelor of Science in Computer Science")
   new Member("John", "assets/MemberImages/John.jpg", "A Cs Student")
 ]
