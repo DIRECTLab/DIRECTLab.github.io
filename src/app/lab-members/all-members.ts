addf910a9d7df2dabfd370c9de7a375ae81a5148
PlipiKunz <>
2021-08-16 21:39:07 -0600

    FIxxed typo.

--- src/app/lab-members/all-members.ts
+++ src/app/lab-members/all-members.ts
@@ -2,5 +2,5 @@ import { Member } from "./member";
 
 export const allMembers: Member[] = [
   new Member("John", "assets/MemberImages/John.jpg", "A graduate Cs Student and web developer cur"),
-  new Member("Carter", "assets/MemberImages/Carter.jpg", "An undergraduate student pursuing a Bachelor of Science in Computer Science")
+  new Member("Carter", "assets/MemberImages/Carter.png", "An undergraduate student pursuing a Bachelor of Science in Computer Science")
 ]
