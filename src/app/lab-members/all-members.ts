33f3f59109290a334e63e63bf82b39a9b319abaa
PlipiKunz <>
2021-08-16 21:47:16 -0600

    Updated bio.

--- src/app/lab-members/all-members.ts
+++ src/app/lab-members/all-members.ts
@@ -1,6 +1,6 @@
 import { Member } from "./member";
 
 export const allMembers: Member[] = [
-  new Member("John", "assets/MemberImages/John.jpg", "A graduate Cs Student and web developer cur"),
+  new Member("John", "assets/MemberImages/John.jpg", "A graduate Cs Student with interests in robotics, web development, and game development. Developer of this site."),
   new Member("Carter", "assets/MemberImages/Carter.png", "An undergraduate student pursuing a Bachelor of Science in Computer Science")
 ]
