9c154f52cd76e5500737d08f7484e8776df9eec7
PlipiKunz <>
2021-08-31 10:12:15 -0600

    Updated members.

--- src/app/lab-members/all-members.ts
+++ src/app/lab-members/all-members.ts
@@ -1,6 +1,8 @@
 import { Member } from "./member";
 
 export const allMembers: Member[] = [
-  new Member("John", "assets/MemberImages/John.jpg", "A graduate Cs Student with interests in robotics, web development, and game development. Developer of this site."),
-  new Member("Carter", "assets/MemberImages/Carter.png", "An undergraduate student pursuing a Bachelor of Science in Computer Science")
+  new Member("Johnathan Kunz", "assets/MemberImages/John.jpeg", "A graduate Cs Student with interests in robotics, web development, and game development. Developer of this site."),
+  new Member("Carter", "assets/MemberImages/Carter.png", "An undergraduate student pursuing a Bachelor of Science in Computer Science"),
+  new Member("Ryan", "assets/MemberImages/RyanZ.png", "")
+
 ]
